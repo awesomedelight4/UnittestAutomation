f1012960f2e0d9143fbfc81a56ba3cd40c611376
awesomedelight4 <awesomedelight4@gmail.com>
2025-11-19 14:00:17 +0100

    calculator.py completed and tests_calculator.py written

--- calculator.py
+++ calculator.py
@@ -1,2 +1,16 @@
-Class Calculator:
-'''Simple calculator performing basic arithmetic operations'''
+class Calculator:
+    """Simple calculator performing basic arithmetic operations."""
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a/b
